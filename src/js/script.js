4338f3a55dc59ab769b3b511d555386b36d737a0
5tka <ferol2010@yandex.ru>
2017-02-23 19:27:21 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+console.log('hello from script.js');
+
+$(document).ready(function() { // начало document.ready
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // конец document.ready
---
+++ src/js/vendor/vendor.js
@@ -0,0 +1,2 @@
+console.log('hello from vendor.js');
+//="../../../bower_components/jquery/dist/jquery.js"
